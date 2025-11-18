6fc5de273956e10e3622a553dd10ed7fc512281b
seongman Joe <smjoe0302@naver.com>
2025-11-18 15:43:45 +0900

    전통적인 JDBC 방법

---
+++ src/test/java/com/codeit/jpastudy/chap01/repository/JdbcProductRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.codeit.jpastudy.chap01.repository;
+
+import com.codeit.jpastudy.chap01.entity.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class JdbcProductRepositoryTest {
+
+    @Autowired
+    private JdbcProductRepository repository;
+
+    @Test
+    void insertTest() {
+        repository.insertProduct("아이폰", 1500000, Product.Category.ELECTRONIC);
+    }
+
+    @Test
+    void selectAllTest() {
+        List<Product> products = repository.selectAllProducts();
+        for (Product product : products) {
+            System.out.println(product);
+        }
+    }
+}
