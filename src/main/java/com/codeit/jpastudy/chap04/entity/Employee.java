6e1da27198e06f75b84fdaf3c0988110f2cf0a97
seongman Joe <smjoe0302@naver.com>
2025-11-19 16:07:17 +0900

    - 단방향 연관관계 확인, FetchType Eager와 Lazy의 차이 확인

--- src/main/java/com/codeit/jpastudy/chap04/entity/Employee.java
+++ src/main/java/com/codeit/jpastudy/chap04/entity/Employee.java
@@ -21,7 +21,9 @@ public class Employee {
     private String name;
 
     // 하나의 부서는 여러 사원을 가질 수 있다.
-    @ManyToOne
+    // EAGER : 해당 필드를 사용 하든 말든 간에 무조건 조인 수행.
+    // LAZY : 필요한 경우에만 데이터를 가져온다 -> 실무
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dept_id") // FK 컬럼명 (연관 테이블의 컬럼명과 일치하게)
     private Department department;
 
